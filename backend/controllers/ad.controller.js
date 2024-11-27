9634d0a30352f02320efa255111a9b627afe8042
Anatoly <Anatoly.Shilyaev@gmail.com>
2024-11-27 14:56:05 +0300

    add decodeObjectSid test-2

--- backend/controllers/ad.controller.js
+++ backend/controllers/ad.controller.js
@@ -35,7 +35,7 @@ export const ADController = {
       let { searchEntries } = await client.search("dc=sfurf,dc=office", opts);
 
       searchEntries = searchEntries.map((entry) => {
-        return { ...entry, objectSid: decodeObjectSid(entry.objectSid) };
+        return { ...entry, objectSid: sidToString(entry.objectSid) };
       });
 
       responce.json({ searchEntries });
@@ -73,16 +73,55 @@ export const ADController = {
   },
 };
 
-function decodeObjectSid(buffer) {
-  let sid = "S-";
-  // Первый байт — это версия
-  sid += buffer[0].toString();
-  // Следующие 6 байтов — это идентификатор органа (Authority ID)
-  sid += "-" + buffer.readUIntBE(2, 6).toString();
-  // Оставшиеся байты — это идентификаторы под-авторитетов (Sub-authorities)
-  const subAuthorityCount = buffer[1];
-  for (let i = 0; i < subAuthorityCount; i++) {
-    sid += "-" + buffer.readUInt32LE(8 + i * 4).toString();
-  }
-  return sid;
-}
+const sidToString = (base64) => {
+  //Конвертируем строку base64 в Buffer, а потом его преобразуем в HEX
+  const buffer = Buffer.from(base64, "base64");
+  const array = buffer.toString("hex"); //010500000000000515000000e967bb98d6b7d7bf82051e6c28060000
+  const G = array.toString().match(/.{1,2}/g);
+
+  /* G array
+    [
+      '01', '05', '00', '00', '00',
+      '00', '00', '05', '15', '00',
+      '00', '00', 'e9', '67', 'bb',
+      '98', 'd6', 'b7', 'd7', 'bf',
+      '82', '05', '1e', '6c', '28',
+      '06', '00', '00'
+    ]
+    */
+
+  const BESA2 = `${G[8]}${G[9]}${G[10]}${G[11]}`;
+  const BESA3 = `${G[12]}${G[13]}${G[14]}${G[15]}`;
+  const BESA4 = `${G[16]}${G[17]}${G[18]}${G[19]}`;
+  const BESA5 = `${G[20]}${G[21]}${G[22]}${G[23]}`;
+  const BERID = `${G[24]}${G[25]}${G[26]}${G[27]}`;
+  const LESA1 = `${G[2]}${G[3]}${G[4]}${G[5]}${G[6]}${G[7]}`;
+
+  const LESA2 = `${BESA2.substr(6, 2)}${BESA2.substr(4, 2)}${BESA2.substr(
+    2,
+    2
+  )}${BESA2.substr(0, 2)}`;
+  const LESA3 = `${BESA3.substr(6, 2)}${BESA3.substr(4, 2)}${BESA3.substr(
+    2,
+    2
+  )}${BESA3.substr(0, 2)}`;
+  const LESA4 = `${BESA4.substr(6, 2)}${BESA4.substr(4, 2)}${BESA4.substr(
+    2,
+    2
+  )}${BESA4.substr(0, 2)}`;
+  const LESA5 = `${BESA5.substr(6, 2)}${BESA5.substr(4, 2)}${BESA5.substr(
+    2,
+    2
+  )}${BESA5.substr(0, 2)}`;
+  const LERID = `${BERID.substr(6, 2)}${BERID.substr(4, 2)}${BERID.substr(
+    2,
+    2
+  )}${BERID.substr(0, 2)}`;
+
+  const LE_SID_HEX = `${LESA1}-${LESA2}-${LESA3}-${LESA4}-${LESA5}-${LERID}`;
+
+  const ADDR = LE_SID_HEX.split("-");
+
+  const SID = "S-1-" + ADDR.map((x) => parseInt(x, 16)).join("-");
+  return SID;
+};
