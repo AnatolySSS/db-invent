6baa09753b000ac60bd7ac21235abce41c1fcf5c
Anatoly <Anatoly.Shilyaev@gmail.com>
2024-12-06 18:41:00 +0300

    test

--- backend/controllers/employers.controller.js
+++ backend/controllers/employers.controller.js
@@ -21,7 +21,7 @@ export const EmployersController = {
       const opts = {
         sizeLimit: 1000,
         scope: "sub",
-        filter: "(&(objectClass=person)(title=*)(dn=*User Accounts*))",
+        filter: "(&(objectClass=person)(title=*)(ou=User Accounts))",
         explicitBufferAttributes: ["objectSid"],
         // attributes: [
         //   "cn",
