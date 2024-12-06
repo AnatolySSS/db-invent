0a2a087bd9ce3d4ec3e09b1a710bf742ecda1cf5
Anatoly <Anatoly.Shilyaev@gmail.com>
2024-12-06 15:08:18 +0300

    test

--- backend/controllers/employers.controller.js
+++ backend/controllers/employers.controller.js
@@ -24,7 +24,7 @@ export const EmployersController = {
         filter: "(&(objectClass=person)(title=*))",
         explicitBufferAttributes: ["objectSid"],
         attributes: [
-          "cn",
+          ["cn", "full_name"],
           "telephoneNumber",
           "mail",
           "mailNickname",
